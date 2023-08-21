3625902b240bf571dec26d15e0914763218ad950
hycel <theunitax@gmail.com>
2023-08-21 15:26:40 +0200

    some changes

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,20 +1,21 @@
 import { Button } from "./Button";
+import { ProfileImage } from "./ProfileImage";
 
 export function NewTweetForm() {
   return (
     <>
       <form className="mx-4 my-2 flex flex-col gap-2 border-b">
         NEW
-        <div className="flex justify-around gap-4">
-          {/* <ProfileImage src="#url" /> */}
+        <div className="flex gap-4">
+          <ProfileImage src="#url" />
           <textarea
             className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
             placeholder="Co tam słychać, mordo?"
           ></textarea>
         </div>
-        <div className="flex justify-around">
-          <Button>Tweet</Button>
-        </div>
+        {/* <div className="flex justify-around"> */}
+        <Button className="self-end">Tweet</Button>
+        {/* </div> */}
       </form>
     </>
   );
---
+++ src/components/ProfileImage.tsx
@@ -0,0 +1,8 @@
+type ProfileImageProps = {
+  src?: string | null;
+  className?: string;
+};
+
+export function ProfileImage({ src, className = "" }: ProfileImageProps) {
+  return <h1> sdf</h1>;
+}
